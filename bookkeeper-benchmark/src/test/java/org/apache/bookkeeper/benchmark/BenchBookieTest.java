be3c80c6daba728e8426c4b4afc78cf5674dfdcc
luca <lucafalasca08@gmail.com>
2023-06-20 12:24:13 +0200

    First Dummy test

--- bookkeeper-benchmark/src/test/java/org/apache/bookkeeper/benchmark/BenchBookieTest.java
+++ bookkeeper-benchmark/src/test/java/org/apache/bookkeeper/benchmark/BenchBookieTest.java
@@ -1,4 +1,13 @@
 package org.apache.bookkeeper.benchmark;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class BenchBookieTest {
+
+    @Test
+    public void testState() {
+        Assert.assertEquals(true, true);
+    }
+
 }
